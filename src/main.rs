ffac340212c6456427b6ac8f768e028638565f39
Mohsen Alizadeh <mohsen@alizadeh.us>
2021-12-23 00:21:05 +0100

    add clap with a simple config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("Todoist Cli")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("Todoist Cli")
+        .subcommand(SubCommand::with_name("list").about("List tasks"))
+        .subcommand(SubCommand::with_name("show").about("Show a task"))
+        .subcommand(SubCommand::with_name("projects").about("List of projects"))
+        .arg(Arg::with_name("file_name"))
+        .get_matches();
+
+    match app.subcommand() {
+        ("list", Some(list_sub)) => println!("list selected"),
+        _ => println!("no command selected"),
+    }
 }
