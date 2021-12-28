374da12e45a199338f0fd8ed0379a1db61d3855e
Mohsen Alizadeh <mohsen@alizadeh.us>
2021-12-28 18:16:09 +0100

    make subcommand required

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::collections::HashMap;
 
 mod client;
@@ -6,13 +6,13 @@ mod commands;
 
 fn main() {
     let app = App::new("Todoist Cli")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("Todoist Cli")
         .subcommand(SubCommand::with_name("list").about("List tasks"))
         .subcommand(SubCommand::with_name("show").about("Show a task"))
         .subcommand(SubCommand::with_name("projects").about("List of projects"))
-        .arg(Arg::with_name("file_name"))
         .get_matches();
 
     match app.subcommand() {
