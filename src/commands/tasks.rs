6466f6d009494457220178412c303336a447c956
Mohsen Alizadeh <mohsen@alizadeh.us>
2021-12-29 16:44:08 +0100

    add task show

--- src/commands/tasks.rs
+++ src/commands/tasks.rs
@@ -1,6 +1,7 @@
 use prettytable::{format, Table};
 
 use crate::client::Client;
+use crate::client::Task;
 
 pub fn list() {
     let client = Client::new();
@@ -13,6 +14,36 @@ pub fn list() {
     for task in client.list() {
         table.add_row(row![task.id, task.content]);
     }
+
+    table.printstd();
+}
+
+pub fn show(task_id: isize) {
+    let client = Client::new();
+
+    let body = client.get(format!("/rest/v1/tasks/{}", task_id).as_str());
+
+    let task: Task = serde_json::from_str(&body).unwrap();
+
+    let mut table = Table::new();
+
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+    table.add_row(row!["id", task.id]);
+    table.add_row(row!["assigner", task.assigner]);
+    table.add_row(row!["project_id", task.project_id]);
+    table.add_row(row!["section_id", task.section_id]);
+    table.add_row(row!["order", task.order]);
+    table.add_row(row!["content", task.content]);
+    table.add_row(row!["description", task.description]);
+    table.add_row(row!["completed", task.completed]);
+    // TODO: table.add_row(row!["label_ids", task.label_ids]);
+    table.add_row(row!["priority", task.priority]);
+    table.add_row(row!["comment_count", task.comment_count]);
+    table.add_row(row!["creator", task.creator]);
+    table.add_row(row!["created", task.created]);
+    // TODO: table.add_row(row!["due", task.due]);
+    table.add_row(row!["url", task.url]);
 
     table.printstd();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate prettytable;
 
-use clap::{App, AppSettings, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 mod client;
 mod commands;
@@ -15,13 +15,21 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("Todoist Cli")
         .subcommand(SubCommand::with_name("list").about("List tasks"))
-        .subcommand(SubCommand::with_name("show").about("Show a task"))
+        .subcommand(
+            SubCommand::with_name("show")
+                .about("Show a task")
+                .arg(Arg::with_name("task_id")),
+        )
         .subcommand(SubCommand::with_name("projects").about("List of projects"))
         .get_matches();
 
     match app.subcommand() {
         ("list", Some(_)) => tasks::list(),
         ("projects", Some(_)) => projects::list(),
+        ("show", Some(args)) => {
+            let id: isize = args.value_of("task_id").unwrap().parse().unwrap();
+            tasks::show(id)
+        }
         _ => (),
     }
 }
